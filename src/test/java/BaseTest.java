9afdc27a2af1cd13824a2364b4a79be6a8104735
Aleksei Korolev <alexkorolev86@gmail.com>
2023-11-25 19:37:24 +0300

    Parametrized tests

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 
 public class BaseTest {
+    @BeforeAll
+    public static void beforeMethod(){
+        Configuration.baseUrl = "https://www.zacks.com/";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
 }
+
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,4 +1,62 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-public class MainTest {
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class MainTest extends BaseTest{
+
+    @ParameterizedTest
+    @DisplayName("Проверка поиска всех акций на zack.com")
+    @Tag("BLOCKER")
+    @CsvFileSource(resources = "allListUnCert.cvs")
+    void checkStocksOnZackCsv(String stock) {
+
+        open("");
+        $("#search-q").click();
+        $("#search-q").setValue(stock).pressEnter();
+        $("#quote_ribbon_v2").shouldHave(text(stock));
+
+    }
+
+    @ParameterizedTest
+    @DisplayName("Проверка поиска крупных акций из индекса QQQ на zack.com")
+    @Tag("CRITICAL")
+    @ValueSource(strings = {"MSFT","AAPL","TSLA","NVDA",
+            "AMD","META","AMZN","NFLX"})
+    void checkStocksOnZackSource(String stock) {
+
+        open("");
+        $("#search-q").click();
+        $("#search-q").setValue(stock).pressEnter();
+        $("#quote_ribbon_v2").shouldHave(text(stock));
+
+    }
+
+    @ParameterizedTest
+    @DisplayName("Проверка отображения имени компаний на zack.com")
+    @Tag("MAJOR")
+    @CsvSource(value = {
+            "MSFT , Microsoft",
+            "AAPL , Apple",
+            "TSLA , Tesla",
+            "NVDA , NVIDIA",
+            "AMD , Advanced Micro Devices",
+            "META , Meta Platforms",
+            "AMZN , Amazon.com",
+            "NFLX , Netflix",
+    })
+    void checkStocksOnZackCsvSource(String stock, String fullNameCompany) {
+
+        open("");
+        $("#search-q").click();
+        $("#search-q").setValue(stock).pressEnter();
+        $("#quote_ribbon_v2").shouldHave(text(fullNameCompany));
+
+    }
 }
