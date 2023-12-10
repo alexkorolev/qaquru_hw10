9ecf69d7badaae5c74dd02e50abb6af3fa840bc9
Aleksei Korolev <alexkorolev86@gmail.com>
2023-12-10 12:51:18 +0300

    Parametrized tests

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,15 +1,24 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 
 public class MainTest extends BaseTest{
+
+    private static Stream<Arguments> provideStockStrings() {
+        return Stream.of(
+                Arguments.of("MSFT" , "Microsoft"),
+                Arguments.of("AAPL" , "Apple"),
+                Arguments.of("TSLA" , "Tesla"),
+                Arguments.of("NVDA" , "NVIDIA")
+        );
+    }
 
     @ParameterizedTest
     @DisplayName("Проверка поиска всех акций на zack.com")
@@ -52,6 +61,19 @@ public class MainTest extends BaseTest{
             "NFLX , Netflix",
     })
     void checkStocksOnZackCsvSource(String stock, String fullNameCompany) {
+
+        open("");
+        $("#search-q").click();
+        $("#search-q").setValue(stock).pressEnter();
+        $("#quote_ribbon_v2").shouldHave(text(fullNameCompany));
+
+    }
+
+    @ParameterizedTest
+    @DisplayName("Проверка отображения имени компаний на zack.com")
+    @Tag("MAJOR")
+    @MethodSource ("provideStockStrings")
+    void checkStocksOnZackMethodSource(String stock, String fullNameCompany) {
 
         open("");
         $("#search-q").click();
